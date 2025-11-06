8ad837d147168a570966e6b147afae8e4edf0477
Sumit Goyal <goyalsumit651@gmail.com>
2025-11-06 18:33:17 +0530

    Enable CORS with a wildcard origin for all requests

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin : '*'
+}));
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
